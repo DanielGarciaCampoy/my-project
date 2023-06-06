1c7145b53af767c957f8332044df85ff6ce27476
Daniel García Campoy <daniel.garcia.campoy@gmail.com>
2023-06-06 11:00:19 +0200

    first commit

---
+++ src/api/cliente-producto/controllers/cliente-producto.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cliente-producto controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cliente-producto.cliente-producto');
---
+++ src/api/cliente-producto/routes/cliente-producto.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cliente-producto router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::cliente-producto.cliente-producto');
